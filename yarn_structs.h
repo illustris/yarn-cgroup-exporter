fbc4797f5cb4c002dde3ac10acca0f821f58b8ec
illustris <rharikrishnan95@gmail.com>
2020-11-12 15:13:55 +0530

    clean up json output

--- yarn_structs.h
+++ yarn_structs.h
@@ -5,29 +5,30 @@
 
 struct app
 {
-	unsigned long long int cluster_timestamp;
+	unsigned long long int cluster_timestamp; // ms
 	unsigned int id;
 	char user[64];
 	char name[128];
 	char queue[128];
-	unsigned long long int started_time;
+	unsigned long long int started_time; // ms
 	char type[32];
 };
 
 struct cnt
 {
-	unsigned int epoch;
-	unsigned long long int cluster_timestamp;
+	unsigned int epoch; // ms
+	unsigned long long int cluster_timestamp; // ms
 	unsigned int app_id;
 	unsigned int attempt_id;
 	unsigned int id;
 
-	unsigned long long int mem_allocated;
+	unsigned long long int mem_allocated; // bytes
 	unsigned int cores_allocated;
-	unsigned long long int started_time;
+	unsigned long long int started_time; // ms
 
-	unsigned long long int cpu_time;
-	unsigned long long int rss;
+	// TODO: convert to ms before storing into struct
+	unsigned long long int cpu_time; // nanoseconds
+	unsigned long long int rss; // bytes
 
 	struct gc_metrics gcm;
 };
